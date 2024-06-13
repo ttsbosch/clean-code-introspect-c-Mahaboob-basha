e0ac0b1130984b0c60dc596cadda477d34b703d5
Mahaboob-basha <58513560+Mahaboob-basha@users.noreply.github.com>
2024-06-13 16:41:56 +0530

    Update and rename TradeProcessor.cpp to CSVToXMLTradeConversion.cpp
    Renamed the filename to specific filename

--- CSVToXMLTradeConversion.cpp
+++ CSVToXMLTradeConversion.cpp
@@ -3,64 +3,6 @@
 #include <string.h>
 #include <errno.h>
 
-typedef struct {
-    char SourceCurrency[256];
-    char DestinationCurrency[256];
-    float Lots;
-    double Price;
-} TradeRecord;
-
-
-char** SplitString(const char* inputString, char delimiter) {
-    int delimiterCount = 0;
-    const char* pointer = inputString;
-    while (*pointer != '\0') {
-        if (*pointer++ == delimiter) {
-            delimiterCount++;
-        }
-    }
-
-    char** referenceOfStringReference = (char**)malloc(sizeof(char*) * (delimiterCount + 2));
-    int delimiterIndex = 0;
-    pointer = inputString;
-    char* stringReference = (char*)malloc(strlen(inputString) + 1);
-    int inputStringIndex = 0;
-    while (*pointer != '\0') {
-        if (*pointer == delimiter) {
-            stringReference[inputStringIndex] = '\0';
-            referenceOfStringReference[delimiterIndex++] = strdup(stringReference);
-            inputStringIndex = 0;
-        } else {
-            stringReference[inputStringIndex++] = *pointer;
-        }
-        pointer++;
-    }
-    stringReference[inputStringIndex] = '\0';
-    referenceOfStringReference[delimiterIndex++] = strdup(stringReference);
-    referenceOfStringReference[delimiterIndex] = NULL;
-    free(stringReference);
-    return referenceOfStringReference;
-}
-
-
-bool TryConvertToIntegerFromString(const char* inputString, int* value) {
-    char* endPointer;
-    *value = strtol(inputString, &endPointer, 10);
-    if (endPointer == inputString) {
-        return 0;
-    }
-    return 1;
-}
-
-bool TryConvertTDoubleFromString(const char* inputString, double* value) {
-    char* endPointer;
-    *value = strtod(inputString, &endPointer);
-    if (endPointer == inputString) {
-        return 0;
-    }
-    return 1;
-}
-
 void CovertCSVToXML(FILE* stream) {
     char line[1024];
     TradeRecord tradeObjects[1024];
