3dc10c84fb4c3cbd779d4e249065337a781d8998
Mahaboob-basha <58513560+Mahaboob-basha@users.noreply.github.com>
2024-06-26 16:24:06 +0530

    Update Converter.h

--- Converter.h
+++ Converter.h
@@ -2,18 +2,11 @@
 bool TryConvertToIntegerFromString(const char* inputString, int* value) {
     char* endPointer;
     *value = strtol(inputString, &endPointer, 10);
-    if (endPointer == inputString) {
-        return 0;
-    }
-    return 1;
+    return endPointer != inputString && *endPointer == '\0';
 }
 
-bool TryConvertTDoubleFromString(const char* inputString, double* value) {
+bool TryConvertToDoubleFromString(const char* inputString, double* value) {
     char* endPointer;
     *value = strtod(inputString, &endPointer);
-    if (endPointer == inputString) {
-        return 0;
-    }
-    return 1;
+    return endPointer != inputString && *endPointer == '\0';
 }
-
