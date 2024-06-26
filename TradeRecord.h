e95c37c5dc2839d2f0c29877982fc6db98474363
Mahaboob-basha <58513560+Mahaboob-basha@users.noreply.github.com>
2024-06-26 14:33:54 +0530

    Update TradeRecord.h

--- TradeRecord.h
+++ TradeRecord.h
@@ -1,7 +1,9 @@
+#define SOURCE_CURRENCY_LENGTH 255
+#define DESTINATION_CURRENCY_LENGTH 255
 
 typedef struct {
-    char SourceCurrency[256];
-    char DestinationCurrency[256];
+    char SourceCurrency[SOURCE_CURRENCY_LENGTH + 1];
+    char DestinationCurrency[DESTINATION_CURRENCY_LENGTH + 1];
     float Lots;
     double Price;
 } TradeRecord;
